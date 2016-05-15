c23a325f4d7baa09963477c6bd370c666a5e0c35
Miles Nosler <mnosler@gmail.com>
2016-05-15 15:08:46 -0500

    tinker with min_samples_split

--- quizzes/decision_tree/classifyDT.py
+++ quizzes/decision_tree/classifyDT.py
@@ -3,7 +3,7 @@ def classify(features_train, labels_train):
     ### your code goes here--should return a trained decision tree classifer
     from sklearn import tree
     
-    clf = tree.DecisionTreeClassifier()
+    clf = tree.DecisionTreeClassifier(min_samples_split=50)
     clf.fit(features_train, labels_train)
     
     
